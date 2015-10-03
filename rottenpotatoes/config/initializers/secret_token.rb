401b323a08226a3e9f1ac8e0a267c0a455c15325
ohnary <ohnarya@tamu.edu>
2015-10-03 15:36:05 -0700

    cucumber;

---
+++ rottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = 'b31b723ede96105ed226fdfd21b36ee425a1aa8733f3015c3747d5a59a92cd311f8eaee4c3470eab5188622fc9b57d9836fa9cde48999da5ff742764cdc19da8'
