e7d48ddb131b1141b3b8b963470689276addf987
ohnary <ohnarya@tamu.edu>
2015-10-03 15:58:11 -0700

    test

---
+++ rottenpotatoes/app/models/movie.rb
@@ -0,0 +1,13 @@
+class Movie < ActiveRecord::Base
+  # if it is, nothing has inserted into the table
+  #attr_accessor :title, :rating, :description, :release_date, :director
+
+  def self.all_ratings
+    %w(G PG PG-13 NC-17 R)
+  end
+
+  def self.same_directors(director)
+    Movie.where(:director => director)
+  end  
+end
+
